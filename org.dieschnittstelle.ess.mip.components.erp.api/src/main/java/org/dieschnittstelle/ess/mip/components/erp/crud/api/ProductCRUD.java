391c0a3497ac6e057e7362d4f368646171f3815d
PartyOnWindows <michi13@t-online.de>
2021-07-06 11:21:50 +0200

    AF 1: Versehen des ProductCRUD interfaces mit JAX-RS annotationen zur bereitstellung

--- org.dieschnittstelle.ess.mip.components.erp.api/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/api/ProductCRUD.java
+++ org.dieschnittstelle.ess.mip.components.erp.api/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/api/ProductCRUD.java
@@ -4,22 +4,35 @@ import java.util.List;
 
 import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
 /*
  * TODO MIP+JPA1/2/5:
  * this interface shall be implemented using a RequestScoped CDI bean with an EntityManager.
  * See TouchpointCRUDImpl for an example bean with a similar scope of functionality
  */
-
+@Path("/products") // ich denke das sollte der ähnluche pfad sein wie in den andern übungen
+@Consumes({MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_JSON})
 public interface ProductCRUD {
 
+	@POST
 	public AbstractProduct createProduct(AbstractProduct prod);
 
+	@GET
 	public List<AbstractProduct> readAllProducts();
 
-	public AbstractProduct updateProduct(AbstractProduct update);
+	@PUT
+	@Path("/{id}")
+	public AbstractProduct updateProduct(@PathParam("id") AbstractProduct update);
 
-	public AbstractProduct readProduct(long productID);
+	@GET
+	@Path("/{id}")
+	public AbstractProduct readProduct(@PathParam("id") long productID);
 
-	public boolean deleteProduct(long productID);
+	@DELETE
+	@Path("/{id}")
+	public boolean deleteProduct(@PathParam("id") long productID);
 
 }
